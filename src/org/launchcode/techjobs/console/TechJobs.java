cc9191257d027ae63435b41124431e94965d6707
Madelyn Zamenski <linuxfanatic@tutanota.com>
2018-11-29 18:55:59 -0600

    Add files via upload

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
+
 /**
  * Created by LaunchCode
  */
@@ -39,7 +40,7 @@ public class TechJobs {
 
                 if (columnChoice.equals("all")) {
                     printJobs(JobData.findAll());
-                } else {
+                } else { //one of the other options eg. location / name/ job title
 
                     ArrayList<String> results = JobData.findAll(columnChoice);
 
@@ -61,15 +62,16 @@ public class TechJobs {
                 String searchTerm = in.nextLine();
 
                 if (searchField.equals("all")) {
-                    System.out.println("Search all fields not yet implemented.");
+                    printJobs(JobData.findByValue(searchTerm)); //use printjobs to print everything. FBV to fill that
+                    //System.out.println("Search all fields not yet implemented.");
                 } else {
-                    printJobs(JobData.findByColumnAndValue(searchField, searchTerm));
+                    printJobs(JobData.findByValue(searchTerm));
                 }
             }
         }
     }
 
-    // ﻿Returns the key of the selected item from the choices Dictionary
+    // Returns the key of the selected item from the choices Dictionary
     private static String getUserSelection(String menuHeader, HashMap<String, String> choices) {
 
         Integer choiceIdx;
@@ -111,6 +113,20 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.isEmpty()) {
+            System.out.println("Sorry, no jobs matched your search term.");
+        }
+
+        for (HashMap<String, String> job : someJobs) {
+            System.out.println("\n********");
+            for (String key : job.keySet()) {
+                System.out.println(key + ": " + job.get(key)); //kEYSET HAS A LOWERCASE K!!!!!!!!
+            }
+            System.out.println("********");
+            
+        }
+        // System.out.println(someJobs);
     }
+
+
 }
